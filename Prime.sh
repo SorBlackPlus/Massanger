36f45316ac028352ad4bebfcb61b62e7d70795d2
SorBlack <sorblackplus@gmail.com>
2017-04-20 14:11:55 +0430

    Updated By @SorBlack :):|

--- Prime.sh
+++ Prime.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/BDMessenger
+cd $HOME/Massanger
 
 install() {
 		sudo apt-get update
@@ -14,20 +14,20 @@ sudo luarocks install serpent
 }
 
 function print_logo() {
-	green "          ____  ____     _____"
-	green "         |  _ )|  _ \   |_   _|___ ____   __  __"
-	green "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-	green "         |____/|____/     |_|\____/\_____|_/\/\_|"
+        green "          ____   __           _____"                                  
+        green "         | __  )|--|---      |_   _|___ ____   __  __"
+        green "         | _|   |--|           | |/ .__|  _ \_|  \/  |"
+        green "         | _|   |--|           |_|\____/\_____|_/\/\_|"
 	echo -e "\n\e[0m"
 }
 
 function logo_play() {
     declare -A txtlogo
     seconds="0.010"
-    txtlogo[1]=" ____  ____     _____"
-    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
-    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
+    txtlogo[1]=" ____   __        _____"
+    txtlogo[2]="|  __ )|--|---   |_   _|___ ____   __  __"
+    txtlogo[3]="| _|   |--|        | |/ .__|  _ \_|  \/  |"
+    txtlogo[4]="| _|   |--|        |_|\____/\_____|_/\/\_|"
     printf "\e[31m\t"
     for i in ${!txtlogo[@]}; do
         for x in `seq 0 ${#txtlogo[$i]}`; do
@@ -42,12 +42,12 @@ function logo_play() {
 
 function beyondteam() {
 	echo -e "\e[0m"
-	green "     >>>>                       We Are Not Attacker                             "
-	green "     >>>>                       We Are Not Alliance                             "
+	green "     >>>>                       We Are (Anonymous)                              "
+	green "     >>>>                       We Are Not Clever                               "
 	white "     >>>>                       We Are Programmer                               "
 	white "     >>>>                       We Are The Best                                 "
 	red   "     >>>>                       We Are Family                                   "
-	red   "     >>>>                       @BeyondTeam                                     "
+	red   "     >>>>                       @PrimeTeam                                     "
 	echo -e "\e[0m"
 }
 
@@ -66,19 +66,19 @@ update() {
 
 if [ "$1" = "install" ]; then
 	print_logo
-	beyondteam
+	Primeteam
 	logo_play
 	install
 elif [ "$1" = "update" ]; then
 	logo_play
-	beyondteam
+	Primeteam
 	update
 	exit 1
 else
 	print_logo
-	beyondteam
+	Primeteam
 	logo_play
-	green "Beyond Messenger Bot running..."
+	green "Prime Messenger Bot running..."
 	#sudo service redis-server restart
 	lua ./bot/bot.lua
 fi
